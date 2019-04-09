cd82dad7485c7d69a2538b8ddf6e4ad953d6a772
Swaggerloaf <swaggerloaf@gmail.com>
2019-04-09 19:56:22 -0400

    Update TodoContext.cs

--- Data/TodoContext.cs
+++ Data/TodoContext.cs
@@ -3,11 +3,16 @@ using todo_starter_netcore_2.Data.Entities;
 
 namespace todo_starter_netcore_2.Data
 {
-  public class TodoContext : DbContext
+  public class TodoContext : IdentityDbContext
   {
     public DbSet<Todo> Todos { get; set; }
     public DbSet<Todo> Tasks { get; set; }
     public DbSet<User> Users { get; set; }
+    
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
